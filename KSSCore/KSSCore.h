02d8ece178972f9a6af74abdca13a65a947a37db
Steven W. Klassen <klassens@acm.org>
2019-04-01 20:29:34 -0600

    Initial version

---
+++ KSSCore/KSSCore.h
@@ -0,0 +1,19 @@
+//
+//  KSSCore.h
+//  KSSCore
+//
+//  Created by Steven W. Klassen on 2019-04-01.
+//  Copyright © 2019 Klassen Software Solutions. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KSSCore.
+FOUNDATION_EXPORT double KSSCoreVersionNumber;
+
+//! Project version string for KSSCore.
+FOUNDATION_EXPORT const unsigned char KSSCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSSCore/PublicHeader.h>
+
+
