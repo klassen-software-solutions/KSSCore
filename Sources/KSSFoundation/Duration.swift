c3f162889483656d9e51c4a1ffc0c8f2064370e0
Steven Klassen <klassens@acm.org>
2020-06-16 15:17:01 -0600

    Updated some of the documentation

--- Sources/KSSFoundation/Duration.swift
+++ Sources/KSSFoundation/Duration.swift
@@ -27,11 +27,26 @@ public func duration(_ count: Double, _ unit: DurationUnit) -> TimeInterval {
  30 days. For more accurate durations you will need to use the `Date` and related classes directly.
  */
 public enum DurationUnit: TimeInterval {
+    /// Conversion of one second to a `TimeInterval`
     case seconds = 1.0
+
+    /// Conversion of one minute to a `TimeInterval`
     case minutes = 60.0
+
+    /// Conversion of one hour to a `TimeInterval`
     case hours = 3600.0
+
+    /// Conversion of one day to a `TimeInterval`
     case days = 86400.0
+    
+    /// Conversion of one week to a `TimeInterval`
     case weeks = 604800.0
-    case months = 2592000.0     /// 30 days
-    case years = 31536000.0     /// 365 days
+
+    /// Conversion of one month to a `TimeInterval`
+    /// - warning: This conversion considers one month to be 30 days
+    case months = 2592000.0
+
+    /// Conversion of one year to a `TimeInterval`
+    /// - warning: This conversion considers one year to be 365 days
+    case years = 31536000.0
 }
