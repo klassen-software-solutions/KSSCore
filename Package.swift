83a3d1c66ec46af9b2981e5992e72de92436e8ef
Steven Klassen <klassens@acm.org>
2020-09-06 19:57:40 -0600

    Modified the package manager to comment out the items being removed

--- Package.swift
+++ Package.swift
@@ -14,14 +14,14 @@ let package = Package(
             .library(name: "KSSFoundation", targets: ["KSSFoundation"]),
             .library(name: "KSSTest", targets: ["KSSTest"]),
         ]
-#if os(macOS)
-        products.append(contentsOf: [
-            .library(name: "KSSCocoa", targets: ["KSSCocoa"]),
-            .library(name: "KSSMap", targets: ["KSSMap"]),
-            .library(name: "KSSUI", targets: ["KSSUI"]),
-            .library(name: "KSSWeb", targets: ["KSSWeb"]),
-        ])
-#endif
+//#if os(macOS)
+//        products.append(contentsOf: [
+//            .library(name: "KSSCocoa", targets: ["KSSCocoa"]),
+//            .library(name: "KSSMap", targets: ["KSSMap"]),
+//            .library(name: "KSSUI", targets: ["KSSUI"]),
+//            .library(name: "KSSWeb", targets: ["KSSWeb"]),
+//        ])
+//#endif
         return products
     }(),
     dependencies: [],
@@ -32,17 +32,17 @@ let package = Package(
             .testTarget(name: "KSSFoundationTests", dependencies: ["KSSFoundation"]),
             .testTarget(name: "KSSTestTests", dependencies: ["KSSTest"]),
         ]
-#if os(macOS)
-        targets.append(contentsOf: [
-            .target(name: "KSSCocoa", dependencies: ["KSSFoundation"]),
-            .target(name: "KSSUI", dependencies: ["KSSFoundation", "KSSCocoa"]),
-            .target(name: "KSSMap", dependencies: ["KSSFoundation", "KSSCocoa"]),
-            .target(name: "KSSWeb", dependencies: ["KSSFoundation", "KSSCocoa"]),
-            .testTarget(name: "KSSCocoaTests", dependencies: ["KSSCocoa"]),
-            .testTarget(name: "KSSUITests", dependencies: ["KSSUI"]),
-            .testTarget(name: "KSSWebTests", dependencies: ["KSSWeb"]),
-        ])
-#endif
+//#if os(macOS)
+//        targets.append(contentsOf: [
+//            .target(name: "KSSCocoa", dependencies: ["KSSFoundation"]),
+//            .target(name: "KSSUI", dependencies: ["KSSFoundation", "KSSCocoa"]),
+//            .target(name: "KSSMap", dependencies: ["KSSFoundation", "KSSCocoa"]),
+//            .target(name: "KSSWeb", dependencies: ["KSSFoundation", "KSSCocoa"]),
+//            .testTarget(name: "KSSCocoaTests", dependencies: ["KSSCocoa"]),
+//            .testTarget(name: "KSSUITests", dependencies: ["KSSUI"]),
+//            .testTarget(name: "KSSWebTests", dependencies: ["KSSWeb"]),
+//        ])
+//#endif
         return targets
     }()
 )
