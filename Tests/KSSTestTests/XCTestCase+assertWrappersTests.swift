325d06b8f81003c3d12beb31cffd523ec9ce5bad
Steven Klassen <klassens@acm.org>
2021-05-20 20:08:39 -0600

    Added the throws keyword to the assertions
    Also eliminated the linux test manifests and main as they are no longer required.

--- Tests/KSSTestTests/XCTestCase+assertWrappersTests.swift
+++ Tests/KSSTestTests/XCTestCase+assertWrappersTests.swift
@@ -5,21 +5,48 @@ import KSSTest
 class XCTestCaseAssertWrappersTests: XCTestCase {
     func testBoolean() throws {
         assertTrue { true }
+        assertTrue {
+            try fnThatThrows()
+            return true
+        }
         assertFalse { false }
+        assertFalse {
+            try fnThatThrows()
+            return false
+        }
     }
 
     func testEquality() throws {
         assertEqual(to: 10) { 2 * 5 }
+        assertEqual(to: 10) {
+            try fnThatThrows()
+            return 2 * 5
+        }
         assertNotEqual(to: 10) { 3 * 5 }
+        assertNotEqual(to: 10) {
+            try fnThatThrows()
+            return 3 * 5
+        }
     }
 
     func testNil() throws {
         assertNil { nil }
+        assertNil {
+            try fnThatThrows()
+            return nil
+        }
         assertNotNil { 10 }
+        assertNotNil {
+            try fnThatThrows()
+            return 10
+        }
     }
 
     func testThrows() throws {
         assertNoThrow { }
+        assertNoThrow {
+            try fnThatThrows()
+        }
         assertThrows {
             throw MyError.somethingWrong
         }
@@ -30,6 +57,10 @@ class XCTestCaseAssertWrappersTests: XCTestCase {
             throw MyError.somethingElseWrong
         }
     }
+}
+
+func fnThatThrows() throws {
+    // Well, not really. It just declares that it throws.
 }
 
 fileprivate enum MyError: Error {
